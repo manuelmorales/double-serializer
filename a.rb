7f49ef185b1151a337b43b41c48e417edf4b82c6
Manuel Morales <manuelmorales@gmail.com>
2014-10-03 14:33:57 +0100

    hash  of lambdas based approach

--- a.rb
+++ a.rb
@@ -7,27 +7,65 @@ RSpec.configure do |c|
 end
 
 class Serializer
+  attr_accessor :finish_proc
+
   def serialize object
-    simplify(object).to_json
+    finish simplify(object)
+  end
+
+  def []= key, value
+    simplifiers[key]= value
+  end
+
+  def initialize &finish_proc
+    @finish_proc = finish_proc
   end
 
   private
 
+  def finish object
+    finish_proc.call object
+  end
+
   def simplify object
-    case object
-    when Doc then {name: "#{object.name}.#{object.format}"}
-    when Array then 
-      object.inject([]) do |array, value|
-        array << simplify(value)
-      end
-    when Hash then 
-      object.inject({}) do |hash, key_value|
-        key, value = key_value
-        hash[key] = simplify(value)
-        hash
-      end
-    else
-      object
+    simplifiers[object].call object
+  end
+
+  def simplifiers
+    @simplifiers ||= new_simplyfiers
+  end
+
+  def new_simplyfiers
+    simplifiers = {
+      Object => method(:do_nothing),
+      Hash => method(:simplify_hash),
+      Array => method(:simplify_array),
+    }
+    simplifiers.default_proc = method(:match_ancestry)
+    simplifiers
+  end
+
+  def match_ancestry hash, object
+    object.class.ancestors.each do |ancestor|
+      return simplifiers[ancestor] if simplifiers.has_key?(ancestor)
+    end
+  end
+
+  def do_nothing object
+    object
+  end
+
+  def simplify_array object
+    object.inject([]) do |array, value|
+      array << simplify(value)
+    end
+  end
+
+  def simplify_hash object
+    object.inject({}) do |hash, key_value|
+      key, value = key_value
+      hash[key] = simplify(value)
+      hash
     end
   end
 end
@@ -35,6 +73,12 @@ end
 Doc = Struct.new :name, :format
 
 RSpec.describe Serializer do
+  subject do
+    serializer = Serializer.new{|object| object.to_json }
+    serializer[Doc]= lambda{|doc| {name: "#{doc.name}.#{doc.format}"} }
+    serializer
+  end
+
   it 'serializes hashes' do
     original = {'a' => 1}
     result = subject.serialize(original)
@@ -49,7 +93,7 @@ RSpec.describe Serializer do
     expect(result).to eq('{"a":[1,2]}')
   end
 
-  it 'serializes files with name and extension' do
+  it 'serializes files with name and format' do
     original = Doc.new 'report', 'txt'
     result = subject.serialize original
     expect(result).to eq('{"name":"report.txt"}')
