4108152474db7c33f5f5af7cfd48e94e727693d7
Manuel Morales <manuelmorales@gmail.com>
2014-10-03 21:57:56 +0100

    refactored tests

--- spec/double_dispatcher_spec.rb
+++ spec/double_dispatcher_spec.rb
@@ -2,33 +2,44 @@ require_relative 'spec_helper'
 require 'double_dispatcher'
 
 RSpec.describe DoubleDispatcher do
-  before :all do
-    MyClass = Class.new
-  end
+  before(:all){ MyClass = Class.new }
+  let(:dispatcher){ DoubleDispatcher.new }
+  let(:target){ double('target') }
 
-  it 'returns object as is by default' do
-    dispatcher = DoubleDispatcher.new
-    original = Object.new
-
-    result = dispatcher.dispatch original
-    expect(result).to eq(original)
+  it 'returns target as is by default' do
+    result = dispatcher.dispatch target
+    expect(result).to eq(target)
   end
 
   it 'calls the initialization block by default' do
-    dispatcher = DoubleDispatcher.new{|o| o.the_default_message }
-    original = Object.new
-    expect(original).to receive(:the_default_message)
+    dispatcher = DoubleDispatcher.new{|t| t.the_default_message }
+    expect(target).to receive(:the_default_message)
 
-    dispatcher.dispatch original
+    dispatcher.dispatch target
   end
 
-  it 'calls the block matching the class of the object' do
-    dispatcher = DoubleDispatcher.new
-    dispatcher[MyClass] = ->(o){ o.the_message }
-    object = MyClass.new
-    expect(object).to receive(:the_message).and_return(:the_response)
+  it 'returns the result of calling the default block' do
+    dispatcher = DoubleDispatcher.new{|t| t.the_default_message }
+    allow(target).to receive(:the_default_message).and_return(:the_response)
 
-    result = dispatcher.dispatch object
+    result = dispatcher.dispatch target
+    expect(result).to eq(:the_response)
+  end
+
+  it 'calls the block matching the class of the target' do
+    dispatcher[MyClass] = ->(t){ t.the_message }
+    target = MyClass.new
+
+    expect(target).to receive(:the_message)
+    dispatcher.dispatch target
+  end
+
+  it 'returns the result of calling the block' do
+    dispatcher[MyClass] = ->(t){ t.the_message }
+    target = MyClass.new
+    allow(target).to receive(:the_message).and_return(:the_response)
+
+    result = dispatcher.dispatch target
     expect(result).to eq(:the_response)
   end
 end
