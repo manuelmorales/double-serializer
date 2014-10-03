1ad93c04ad8d757a6eb6536347f8f208e93ae730
Manuel Morales <manuelmorales@gmail.com>
2014-10-03 13:52:14 +0100

    Arrays and Hashes

--- a.rb
+++ a.rb
@@ -8,10 +8,26 @@ end
 
 class Serializer
   def serialize object
+    simplify(object).to_json
+  end
+
+  private
+
+  def simplify object
     case object
-    when Doc then {name: "#{object.name}.#{object.format}"}.to_json
+    when Doc then {name: "#{object.name}.#{object.format}"}
+    when Array then 
+      object.inject([]) do |array, value|
+        array << simplify(value)
+      end
+    when Hash then 
+      object.inject({}) do |hash, key_value|
+        key, value = key_value
+        hash[key] = simplify(value)
+        hash
+      end
     else
-      object.to_json
+      object
     end
   end
 end
@@ -37,6 +53,18 @@ RSpec.describe Serializer do
     original = Doc.new 'report', 'txt'
     result = subject.serialize original
     expect(result).to eq('{"name":"report.txt"}')
+  end
+
+  it 'serializes hashes of files correctly' do
+    original = {my_file: Doc.new('report', 'txt')}
+    result = subject.serialize original
+    expect(result).to eq('{"my_file":{"name":"report.txt"}}')
+  end
+
+  it 'serializes arrays of files correctly' do
+    original = [Doc.new('report', 'txt'), Doc.new('audio', 'mp3')]
+    result = subject.serialize original
+    expect(result).to eq('[{"name":"report.txt"},{"name":"audio.mp3"}]')
   end
 end
 
