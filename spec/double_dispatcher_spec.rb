3c0d8699978405439294002b22ff38e97f2ee0a4
Manuel Morales <manuelmorales@gmail.com>
2014-10-03 21:51:04 +0100

    calls the block matching the class of the object

--- spec/double_dispatcher_spec.rb
+++ spec/double_dispatcher_spec.rb
@@ -2,6 +2,10 @@ require_relative 'spec_helper'
 require 'double_dispatcher'
 
 RSpec.describe DoubleDispatcher do
+  before :all do
+    MyClass = Class.new
+  end
+
   it 'returns object as is by default' do
     dispatcher = DoubleDispatcher.new
     original = Object.new
@@ -16,5 +20,15 @@ RSpec.describe DoubleDispatcher do
     expect(original).to receive(:the_default_message)
 
     dispatcher.dispatch original
+  end
+
+  it 'calls the block matching the class of the object' do
+    dispatcher = DoubleDispatcher.new
+    dispatcher[MyClass] = ->(o){ o.the_message }
+    object = MyClass.new
+    expect(object).to receive(:the_message).and_return(:the_response)
+
+    result = dispatcher.dispatch object
+    expect(result).to eq(:the_response)
   end
 end
